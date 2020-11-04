1b9f962abea9a10f1381edda2b1565816472f49c
mingkunshi <shimingkun2017@gmail.com>
2020-11-04 03:51:22 -0500

    Updates

---
+++ precache-manifest.6fc3f5db943627dad12a6be7ba39de4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cda23b083788d0e1c3a8e0a4f939b1e2",
+    "url": "/Covid19_Tracker/index.html"
+  },
+  {
+    "revision": "faf5d97191c0c0d4f235",
+    "url": "/Covid19_Tracker/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "4cfbd4f1b898ea1e7e1d",
+    "url": "/Covid19_Tracker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "faf5d97191c0c0d4f235",
+    "url": "/Covid19_Tracker/static/js/2.9bafaaf2.chunk.js"
+  },
+  {
+    "revision": "9f3e154b5a21336fa161f9ed4e97157c",
+    "url": "/Covid19_Tracker/static/js/2.9bafaaf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cfbd4f1b898ea1e7e1d",
+    "url": "/Covid19_Tracker/static/js/main.d81e26bb.chunk.js"
+  },
+  {
+    "revision": "e6705d65cf4d3f9923ab",
+    "url": "/Covid19_Tracker/static/js/runtime-main.ae2c7aa3.js"
+  }
+]);
